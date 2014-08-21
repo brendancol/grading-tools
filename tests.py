10f76151fc2ca08846a98a4ef571d6046dc78083
Brendan Collins <bcollins@blueraster.com>
2014-08-20 21:26:36 -0400

    add two new tests for excel

--- tests.py
+++ tests.py
@@ -12,11 +12,43 @@ class GradingToolTests(unittest.TestCase):
 	def tearDown(self):
 		pass
 
-	def test_create_histogram(self):
+	def test_create_histogram_csv(self):
 		grades_csv = os.path.join(self.data_directory,'grades.csv')
 		grade_field = 'grades'
-
 		output = os.path.join(self.output_directory, 'test_histogram.png')
+		
+		success = grade_histogram.create_histogram(grades_csv, grade_field, output, 'Grades PNG')
+		self.assertTrue(success)
+
+		output = os.path.join(self.output_directory, 'test_histogram.pdf')
+		success = grade_histogram.create_histogram(grades_csv, grade_field, output, 'Grades PDF')
+		self.assertTrue(success)
+
+		output = os.path.join(self.output_directory, 'test_histogram.jpg')
+		success = grade_histogram.create_histogram(grades_csv, grade_field, output, 'Grades JPG')
+		self.assertTrue(success)
+
+	def test_create_histogram_xls(self):
+		grades_csv = os.path.join(self.data_directory,'grades.xls')
+		grade_field = 'grades'
+		output = os.path.join(self.output_directory, 'test_histogram.png')
+		
+		success = grade_histogram.create_histogram(grades_csv, grade_field, output, 'Grades PNG')
+		self.assertTrue(success)
+
+		output = os.path.join(self.output_directory, 'test_histogram.pdf')
+		success = grade_histogram.create_histogram(grades_csv, grade_field, output, 'Grades PDF')
+		self.assertTrue(success)
+
+		output = os.path.join(self.output_directory, 'test_histogram.jpg')
+		success = grade_histogram.create_histogram(grades_csv, grade_field, output, 'Grades JPG')
+		self.assertTrue(success)
+
+	def test_create_histogram_xlsx(self):
+		grades_csv = os.path.join(self.data_directory,'grades.xlsx')
+		grade_field = 'grades'
+		output = os.path.join(self.output_directory, 'test_histogram.png')
+		
 		success = grade_histogram.create_histogram(grades_csv, grade_field, output, 'Grades PNG')
 		self.assertTrue(success)
 
