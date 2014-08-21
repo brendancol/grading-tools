76189577f5af2280c656f142f5c83fd232a81421
Brendan Collins <bcollins@blueraster.com>
2014-08-20 21:26:16 -0400

    add support for excel

--- grade_histogram.py
+++ grade_histogram.py
@@ -1,9 +1,14 @@
 import os
 import pandas
 
-def create_histogram(csv, histoField, outputPath, title, binCount=30):
-	df = pandas.read_csv(csv)
-	histo = df[histoField].hist(bins=binCount)
+def create_histogram(inputFile, histoField, outputPath, title, binCount=30):
+	if '.csv' in inputFile:
+		dataframe = pandas.read_csv(inputFile)
+	elif '.xls' in inputFile:
+		dataframe = pandas.read_excel(inputFile)
+		
+	data = dataframe[histoField]
+	histo = data.hist(bins=binCount)
 	histo.set_title(title)
 	histo.set_xlim([20,100])
 	histo.set_xlabel('grade')
